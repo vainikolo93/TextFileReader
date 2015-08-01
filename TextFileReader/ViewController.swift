6d723d1e1206009c042a70bb21f36e240113e3f9
Nancy Vainikolo <nancyvainikolo@gmail.com>
2015-08-01 21:38:43 +1200

    No Text Displays
    The App launches but the text from the .txt file isn’t showing

--- TextFileReader/ViewController.swift
+++ TextFileReader/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextView: UITextView!
+    var file = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        TextView.text = file
+        
+        let path = NSBundle.mainBundle().pathForResource("TextFile", ofType: "txt")
+        var data = String(contentsOfFile:path!, encoding: NSUTF8StringEncoding, error: nil)
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
