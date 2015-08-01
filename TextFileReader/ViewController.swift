25092c4778ba68c2b34ea8c66dfacc02f9d9e8a4
Nancy A. Vainikolo <nancyavainikolo@Nancys-MacBook-Pro.local>
2015-08-01 20:49:01 +1200

    Initial Commit

---
+++ TextFileReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextFileReader
+//
+//  Created by Nancy A. Vainikolo on 1/08/15.
+//  Copyright (c) 2015 Nancy A. Vainikolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TextFileReaderTests/TextFileReaderTests.swift
@@ -0,0 +1,36 @@
+//
+//  TextFileReaderTests.swift
+//  TextFileReaderTests
+//
+//  Created by Nancy A. Vainikolo on 1/08/15.
+//  Copyright (c) 2015 Nancy A. Vainikolo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TextFileReaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
